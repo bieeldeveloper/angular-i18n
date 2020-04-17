c2ffa4be6e7fd6bb9d3c1f0d76710bdf14cbf24f
Gabriel Soares <gabriel.soares@inmetrics.com.br>
2020-04-17 10:08:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angularI18n';
+
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'hi', label: 'हिंदी' },
+    { code: 'es', label: 'Espanol' }
+
+  ];
+  
+  constructor(@Inject(LOCALE_ID) protected localeId: string) { }
 }
